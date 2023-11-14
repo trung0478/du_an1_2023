40d7972da62342e0a5884d97f974517ed68d07e7
Luan Nguyen <teonguoinoitieng@gmail.com>
2023-11-14 23:36:29 +0700

    update interface

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,5 @@
 <?php
-include '../admin/view/header.php';
+include 'view/header.php';
 ?>
    
    <?php
@@ -8,66 +8,66 @@ include '../admin/view/header.php';
         switch ($act) {
             // Catalog
             case 'list_catalog':
-                include '../admin/view/catalog/list_catalog.php';
+                include 'view/catalog/list_catalog.php';
                 break;
             case 'add_catalog':
-                include '../admin/view/catalog/add_catalog.php';
+                include 'view/catalog/add_catalog.php';
                 break;
             case 'update_catalog':
-                include '../admin/view/catalog/update_catalog.php';
+                include 'view/catalog/update_catalog.php';
                 break;
 
             // Product
             case 'list_product':
-                include '../admin/view/product/list_product.php';
+                include 'view/product/list_product.php';
                 break;
             case 'add_product':
-                include '../admin/view/product/add_product.php';
+                include 'view/product/add_product.php';
                 break;
             case 'update_product':
-                include '../admin/view/product/update_product.php';
+                include 'view/product/update_product.php';
                 break;
 
             // User
             case 'list_user':
-                include '../admin/view/user/list_user.php';
+                include 'view/user/list_user.php';
                 break;
             case 'add_user':
-                include '../admin/view/user/add_user.php';
+                include 'view/user/add_user.php';
                 break;
             case 'update_user':
-                include '../admin/view/user/update_user.php';
+                include 'view/user/update_user.php';
                 break;
 
             // Comment
             case 'list_cmt':
-                include '../admin/view/comment/list_cmt.php';
+                include 'view/comment/list_cmt.php';
                 break;
             case 'detail_cmt':
-                include '../admin/view/comment/detail_cmt.php';
+                include 'view/comment/detail_cmt.php';
                 break;
 
             // Order
             case 'list_order':
-                include '../admin/view/order/list_order.php';
+                include 'view/order/list_order.php';
                 break;
 
             // Discount
             case 'list_sale':
-                include '../admin/view/sale/list_sale.php';
+                include 'view/sale/list_sale.php';
                 break;
             case 'add_sale':
-                include '../admin/view/sale/add_sale.php';
+                include 'view/sale/add_sale.php';
                 break;
             case 'update_sale':
-                include '../admin/view/sale/update_sale.php';
+                include 'view/sale/update_sale.php';
                 break;
         }
     } else {
-        include '../admin/view/home.php';
+        include 'view/home.php';
     }
     ?>
     
 <?php
-    include '../admin/view/footer.php';
+    include 'view/footer.php';
 ?>
