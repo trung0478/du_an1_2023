87acc3a63d9104ff66a1c81e2c62d4ec51230ec4
trung0478 <dangquoctrung7804@gmail.com>
2023-11-19 22:54:16 +0700

    update user interface 19/11

--- user/index.php
+++ user/index.php
@@ -1,10 +1,16 @@
 <?php
+session_start();
+$_SESSION['user'] = 'Trung';
 include "config/connectdb.php";
 include "user/model/product.php";
+include "user/model/comment.php";
 include "view/header.php";
 include "global/global.php";
 // Load product - Our product
-$list_product = load_product();
+$list_product = load_product(0);
+
+// Load product discount
+$list_product_discount = load_product(1);
 
 if (isset($_GET['act']) && $_GET['act'] != "") {
     switch ($_GET['act']) {
@@ -41,8 +47,36 @@ if (isset($_GET['act']) && $_GET['act'] != "") {
             break;
 
         case 'product_detail':
+            // add comment
+            if (isset($_POST['add_comment'])) {
+                $noi_dung = $_POST['comment'];
+                $id_nguoidung = 1;
+                if (isset($_SESSION['user'])) {
+                    insert_comment($_GET['id_pro'], $id_nguoidung, $noi_dung);
+                } else {
+                    $message_noLogin = "Bạn chưa đăng nhập - Vui lòng đăng nhập để thực hiện bình luận";
+                }
+            }
+
             if (isset($_GET['id_pro'])) {
-                $product_detail=load_detail_product($_GET['id_pro']);
+                // load product_detail by id_pro
+                $product_detail = load_detail_product($_GET['id_pro']);
+
+                // load color size
+                $load_color_size = load_color_size($_GET['id_pro']);
+
+                // load comments by id_pro
+                $product_comment = load_comment($_GET['id_pro']);
+
+                // load product in the same catalog
+                $product_thesame_catalog = products_in_the_same_catalog($_GET['id_pro'], $product_detail['ma_lsp']);
+
+                // load 10 prouct most view
+                $product_most_view= product_most_view();
+
+                //count comment
+                $count_comment=count_comment_by_idpro($_GET['id_pro']);
+                
                 include "view/product_detail.php";
             }
             break;
--- user/model/product.php
+++ user/model/product.php
@@ -1,17 +1,47 @@
 <?php
-function load_product()
+function load_product($sp_km)
 {
-    $sql = "select * from sanpham where 1 order by ma_sp desc limit 0,9";
+    if ($sp_km == 1) {
+        $sql = "SELECT sp.*, bt.* FROM sanpham sp JOIN bienthe bt ON sp.ma_sp = bt.ma_sp WHERE bt.gia_km != 0 GROUP BY sp.ten_sp";
+    } else {
+        $sql = "SELECT sp.*, bt.* FROM sanpham sp JOIN bienthe bt ON sp.ma_sp = bt.ma_sp GROUP BY sp.ten_sp";
+    }
     $result = pdo_query($sql);
     return $result;
 }
 
 function load_detail_product($id_product)
 {
-    $sql = "select * from sanpham where ma_sp=$id_product";
+    $sql = "SELECT sanpham.*, bienthe.*  FROM sanpham 
+    JOIN bienthe ON sanpham.ma_sp = bienthe.ma_sp 
+    WHERE bienthe.ma_sp=$id_product";
     $result = pdo_query_one($sql);
-    $update_view = $result['luot_xem'];
-    $sql_ = "update sanpham set luot_xem=$update_view where ma_sp=$id_product";
+
+    // update view
+    $update_view = $result['luot_xem'] + 1;
+    $sql_ = "UPDATE sanpham sp SET sp.luot_xem=$update_view WHERE sp.ma_sp=$id_product";
     pdo_execute($sql_);
+
+    return $result;
+}
+
+function load_color_size($id_pro)
+{
+    $sql = "SELECT DISTINCT kich_co.*, mau_sac.* FROM kich_co
+    JOIN bienthe ON bienthe.ma_kich_co = kich_co.ma_kich_co 
+    JOIN mau_sac ON mau_sac.ma_mau= bienthe.ma_mau WHERE bienthe.ma_sp = $id_pro";
+    $result = pdo_query($sql);
+    return $result;
+}
+
+function products_in_the_same_catalog($id_pro, $id_catalod){
+    $sql="SELECT * FROM sanpham sp JOIN bienthe bt ON bt.ma_sp=sp.ma_sp WHERE sp.ma_lsp = $id_catalod AND sp.ma_sp <> $id_pro";
+    $result = pdo_query($sql);
+    return $result;
+}
+
+function product_most_view(){
+    $sql = "SELECT * FROM sanpham sp JOIN bienthe bt ON bt.ma_sp = sp.ma_sp WHERE 1 ORDER BY luot_xem DESC LIMIT 0,10";
+    $result = pdo_query($sql);
     return $result;
 }
