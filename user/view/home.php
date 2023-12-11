3c2c0affd9d88ba70b2f922421449276ada3f1af
Luan Nguyen <teonguoinoitieng@gmail.com>
2023-12-12 03:33:02 +0700

    update

--- user/view/home.php
+++ user/view/home.php
@@ -184,7 +184,6 @@
                
                 foreach ($best_saleProduct as $value) :
                     extract($value);
-                    echo $so_luong;
                 ?>
                     <div class="new-product-item swiper-slide">
                         <div class="product">
